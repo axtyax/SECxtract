a39be892390d35dac1251a8d713b45767f5d5af6
Axtya Darius <barbanod@bxscience.edu>
2019-07-08 13:19:45 -0400

    different library

--- experimental/label_cells.js
+++ experimental/label_cells.js
@@ -1,17 +1,9 @@
-var htmlparser = require("htmlparser");
+var HTMLParser = require('node-html-parser');
 var sys = require("sys");
 var fs = require("fs");
 
-var rawHtml = "Xyz <script language= javascript>var foo = '<<bar>>';< /  script><!--<!-- Waah! -- -->";
-var handler = new htmlparser.DefaultHandler(function (error, dom) {
-    if (error)
-        console.log(error)
-    else
-        console.log("parsing successful...")
-});
-var parser = new htmlparser.Parser(handler);
 fs.readFile("data/msft-test.htm", "utf8", function(err, data) {
     if (err) throw err;
-    parser.parseComplete(data);
-    sys.puts(sys.inspect(handler.dom, false, null));
+    const root = HTMLParser.parse(data);
+    console.log(root);
 });
